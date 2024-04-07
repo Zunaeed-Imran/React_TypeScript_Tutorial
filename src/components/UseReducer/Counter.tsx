cf17c770738ca02b5b1e042090b862f2cf906b23
imran <zunaeede@gmail.com>
2024-04-07 20:58:58 +0600

    main - also done learning about the payload in useReducer hook. - using typescript.

--- src/components/UseReducer/Counter.tsx
+++ src/components/UseReducer/Counter.tsx
@@ -1,15 +1,17 @@
-import React, { useReducer } from 'react'
+import  { useReducer } from 'react'
 
 
 const INCREMENT = "INCREMENT";
+const INCREMENTBYAMOUNT = "INCREMENTBYAMOUNT";
 const RESET = "RESET";
 const DECREMENT = "DECREMENT";
 
 type IncrementType = { type: typeof INCREMENT };
+type IncrementByAmountType = {type: typeof INCREMENTBYAMOUNT, payload: number}
 type ResetType = { type: typeof RESET};
 type DecrementType = { type: typeof DECREMENT};
 
-type CounterActionType = IncrementType | DecrementType | ResetType;
+type CounterActionType = IncrementType | DecrementType | ResetType | IncrementByAmountType;
 
 const initialState = {
   count: 0
@@ -23,6 +25,8 @@ const reducer = (state: counterType, action: CounterActionType) => {
 switch (action.type) {
   case INCREMENT:
     return { count: state.count + 1 }
+  case INCREMENTBYAMOUNT:
+    return { count: state.count + action.payload}
   case RESET:
     return { count: 0 }
   case DECREMENT:
@@ -49,6 +53,9 @@ const Counter = () => {
       <button onClick={() => {
         dispatch({type: DECREMENT})
       }}>Decrement</button>
+      <button onClick={() => {
+        dispatch({ type: INCREMENTBYAMOUNT, payload: 5})
+      }}>Increment By Amount</button>
 
     </>
   )
