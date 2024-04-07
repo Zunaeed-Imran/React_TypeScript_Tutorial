2209be9d7513c027325e26d852bae663a4460e31
imran <zunaeede@gmail.com>
2024-04-07 21:00:59 +0600

    todo - delete all data for new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
 
 import './App.css'
-import UseReducer from './components/UseReducer/UseReducer';
-// import TypingFormEvent from './components/TypingFormEvent/TypingFormEvent';
-// import TypingEvent from './components/TypingEvent/TypingEvent';
-// import TypingUseState from './components/TypingUseState/TypingUseState';
-// import StyleProps from './components/StyleProps/StyleProps'
-// import CHildren_Props from './components/ChildrenProps/CHildren_Props'
-// import UnionProp from './components/UnionPropsType/UnionProp'
-// import Array_Object from './components/ArrayOfObject/Array_Object'
-// import User3 from './components/UserDefineProps/User3'
-// import User from './components/Built_in_Prop/User'
+
 
 function App() {
 
 
   return (
     <>
-      {/* <User/> */}
 
-      {/* <User3/> */}
-
-      {/* <Array_Object/> */}
-
-      {/* <UnionProp/> */}
-
-      {/* <CHildren_Props/> */}
-
-      {/* <StyleProps/> */}
-
-      {/* <TypingUseState/> */}
-
-      {/* <TypingEvent/> */}
-
-      {/* <TypingFormEvent/> */}
-
-      <UseReducer/>
     </>
   );
 }
